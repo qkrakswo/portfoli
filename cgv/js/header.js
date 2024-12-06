17670daa8d8721cac8f8aa9f7788b3bc81e68643
qkrakswo <mrf02@naver.com>
2024-12-06 14:49:36 +0900

    cgv 스크롤 헤더색 변경
    cgv 스크롤 헤더색 변경

--- cgv/js/header.js
+++ cgv/js/header.js
@@ -5,4 +5,13 @@ $(document).ready(function () {
     $(".nav>li").on("mouseout", function () {
         $(".nav_bg").stop().slideUp()
     });
+
+    //스크롤시 헤더색 변경
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()>=200){
+            $('.nav_line').addClass('on');
+        }else{
+            $('.nav_line').removeClass('on');
+        }
+    });
 })
